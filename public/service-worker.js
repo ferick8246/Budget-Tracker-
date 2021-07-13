c7ccbfe0d28ef9caaf59bc972f51d154ea3bff9c
Erick Flore <ferick8246@gmail.com>
2021-07-12 20:05:36 -0500

    service

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,10 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/index.js",
     "/db.js",
-    "/style.css"];
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "manifest.webmanifest",];
 
     const CACHE_NAME = 'static-cache-v13';
     const DATA_CACHE_NAME = 'data-cache-v8';
